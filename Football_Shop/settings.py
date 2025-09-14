9c0edb16db1133241e1b7ba4c84db93c767024aa
bilqisnisrina <bilqismulyadi07@gmail.com>
2025-09-15 00:45:43 +0700

    Bili Shop

--- Football_Shop/settings.py
+++ Football_Shop/settings.py
@@ -32,7 +32,9 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bilqis-nisrina-footballshop.pbp.cs.ui.ac.id"]
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://pbp.cs.ui.ac.id/web/project/bilqis.nisrina/footballshop"
+]
 
 # Application definition
 
@@ -61,7 +63,7 @@ ROOT_URLCONF = 'Football_Shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
