a5af6efa7a3de1e9b90578948b3934397bca175f
bilqisnisrina <bilqismulyadi07@gmail.com>
2025-09-17 01:11:27 +0700

    tugas 3

--- Football_Shop/settings.py
+++ Football_Shop/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bilqis-nisrina-footballshop.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "bilqis-nisrina-bathletica.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
     "https://pbp.cs.ui.ac.id/web/project/bilqis.nisrina/footballshop"
 ]
