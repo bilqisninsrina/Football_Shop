46c0c3ff65d6b6285281c8d9c3552f5131b0d5f2
bilqisnisrina <bilqismulyadi07@gmail.com>
2025-09-17 00:44:09 +0700

    Bili Shop

--- main/models.py
+++ main/models.py
@@ -1,14 +1,28 @@
+import uuid
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('fitness', 'Fitness'),
+        ('apparel', 'Apparel'),
+        ('shoes', 'Shoes'),
+        ('outdoor', 'Outdoor'),
+        ('ball', 'Ball'),
+        ('racket', 'Racket'),
+        ('accessories', 'Accessories'),
+        ('supplements', 'Aupplements'),
+        ('others', 'Lainnya'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField()
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='others')
     is_featured = models.BooleanField(default=False)
     stock = models.IntegerField(default=0)
-    brand = models.CharField(max_length=50, default="Generic")
+    brand = models.CharField(max_length=50, default="Flexora")
 
     def __str__(self):
         return self.name
