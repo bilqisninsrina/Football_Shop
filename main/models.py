5d592467927b58899b6d595034e87cd60d964e11
bilqisnisrina <bilqismulyadi07@gmail.com>
2025-09-17 16:14:40 +0700

    Tugas 4

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 import uuid
 from django.db import models
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     CATEGORY_CHOICES = [
         ('fitness', 'Fitness'),
         ('apparel', 'Apparel'),
